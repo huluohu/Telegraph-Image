82abc30aa086247baa7e11dfab30430fad4b6a90
gxl <hzguxiaolong@corp.netease.com>
2023-09-21 14:50:24 +0800

    perf(上传): 修改响应报文为字典

--- functions/upload.js
+++ functions/upload.js
@@ -14,6 +14,7 @@ export async function onRequestPost(context) {  // Contents of context object
          headers: request.headers,
          body: request.body,
      });
-    return response;
+     //返回第一个地址
+    return response[0];
   }
   
